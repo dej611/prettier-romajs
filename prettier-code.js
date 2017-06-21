218bb49e9b8b3fc1015e67a8a61ab313e725846f
dej611 <dej611@gmail.com>
2017-06-21 09:03:55 +0200

    first commit

---
+++ prettier-code.js
@@ -0,0 +1,17 @@
+if( false ){ foo() }
+
+const f = function fn(a,b,c) { return a + b + c }
+
+const array = ['first', 'second', 'third', 'fourth']; const object = {}
+
+const arrow = ()=>({})
+
+const veryVeryVeryLongVariableName = [{ array, object, f, fn, foo, hello, arrow}];
+
+    function a(){
+        if ( true == false ) {
+                return;
+        } else {
+              'a';
+        }
+    };
